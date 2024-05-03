ae9a469bc2f6ffc4e6cd44fc9367591926b336af
sanjay17s <sanjay.17suresh2000@gmail.com>
2024-05-03 16:33:00 +0530

    refractor

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import ManualDependencyInjection.MyClass;
 import ManualDependencyInjection.MyFactory;
 
+
 public class Main {
     public static void main(String[] args) {
 
         MyClass mc = new MyFactory().createMyClass();
         mc.doSome();
+
+
     }
+
 }
